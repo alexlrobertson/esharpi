464214a2459075cb46517bfbdfffff461e5915fd
Alex Robertson <alexleonardrobertson@gmail.com>
2016-07-30 14:47:20 -0500

    add simple test suite

---
+++ test.js
@@ -0,0 +1,10 @@
+const esharpi = require('./');
+const assert = require('assert');
+
+const example = 'const foo = "bar";';
+
+assert.notEqual(esharpi(example), example, 'does not exactly match input');
+assert(esharpi(example).match(/const/), 'has reference to variable type');
+assert(esharpi(example).match(/foo/), 'has reference to original variable name');
+assert(esharpi(example).match(/bar/), 'has reference to original string value');
+assert(esharpi(example).match(/\u001b/), 'contains ANSI colors');
